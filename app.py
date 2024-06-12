bd32a3674b8ddec1312f21ce4f89811030da7d8a
Samuel Arthur Ameyaw <161239292+arthur-samuel@users.noreply.github.com>
2024-06-12 11:44:34 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,27 +1,53 @@
 import gradio as gr
+import cv2
 import tensorflow as tf
-from tensorflow.keras.models import load_model
 import numpy as np
+from tensorflow.keras.preprocessing.image import ImageDataGenerator # type: ignore
+from tensorflow.keras.models import load_model
+
 # Load the pre-trained model
-model_path = "D:\SKAY\Projects\KEEP\VScode\Brain\model.h5"
-model = load_model(model_path)
+model = tf.keras.models.load_model('Trained_Model.h5')
 
-# Function to preprocess the image
-def preprocess_image(image):
-    image = tf.image.resize(image, (150, 150))  # Resize the image
-    image = tf.expand_dims(image, axis=0)  # Add batch dimension
-    image = image / 255.0  # Normalize pixel values
-    return image
+# Define the class labels
+class_labels = {
+    0: 'Glioma',
+    1: 'Meningioma',
+    3: 'Pituitary',
+    2: 'No tumor'
+}
 
-# Function to make predictions
-def predict_image(image):
-    preprocessed_image = preprocess_image(image)
-    prediction = model.predict(preprocessed_image)
-    # Assuming model output is binary
-    if prediction[0][0] > 0.5:
-        return "Tumor Detected"
+# Create the image generator for preprocessing
+img_gen = ImageDataGenerator(rescale=1./255)
+
+# Define the function to predict tumor type
+def predict_image(file):
+    # Load the image or video
+    cap = cv2.VideoCapture(file.name)
+    if cap.isOpened():
+        ret, frame = cap.read()
+        # Check if it's an image or video
+        if frame is not None:
+            # Preprocess the image            
+            frame = cv2.resize(frame, (150, 150))
+            frame = np.expand_dims(frame, axis=-1)
+            frame = np.expand_dims(frame, axis=0)
+            frame = frame.astype('float32')
+            frame = img_gen.standardize(frame)
+
+            # Predict the tumor type
+            prediction = model.predict(frame)
+            label = class_labels[np.argmax(prediction)]
+        else:
+            label = "No frames found in the video"
     else:
-        return "Healthy Brain"
+        label = "Could not open the file"
+    return label
 
-# Create interface
-gr.Interface(predict_image,'image' ,"label", title="Brain Tumor Detection").launch(share=True)
+# Create the Gradio interface
+input_type = gr.inputs.File(label="Upload an image to predict the brain tumor type")
+output_type = gr.outputs.Textbox(label="Predicted Brain Tumor ")
+title = "Brain Tumor Classification"
+description = "Upload an MRI scan image to predict the corresponding brain tumor type"
+iface = gr.Interface(fn=predict_image, inputs=input_type, outputs=output_type, title=title, description=description)
+if __name__ == '__main__':
+    iface.launch(inline=False)
